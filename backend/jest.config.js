2670640405c94ddffe851b8715a01808de1efa1d
Martin Kalema <kalemaaaaaaaa@gmail.com>
2024-10-15 15:48:21 +0300

    [chore] created backend folder

---
+++ backend/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  testMatch: ['**/__tests__/**/*.test.js', '**/?(*.)+(spec|test).js'],
+};
