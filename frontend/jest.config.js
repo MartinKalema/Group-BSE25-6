352b9073e112721dcd6a2a378ba0feeea2dac5d2
Martin Kalema <kalemaaaaaaaa@gmail.com>
2024-10-17 01:01:26 +0300

    [chore] clean up

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,13 +1,15 @@
 module.exports = {
-    transform: {
-      "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
-    },
-    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-    testEnvironment: "jsdom", 
-    transformIgnorePatterns: ["/node_modules/"],
-    collectCoverage: true,
-    coverageDirectory: 'coverage',
-    coverageReporters: ['text', 'lcov'],
-    testMatch: ['**/__tests__/*.test.js']
-  };
-  
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  testEnvironment: 'jsdom',
+  transformIgnorePatterns: ['/node_modules/'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  testMatch: [
+    '**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '**/*.{spec,test}.{js,jsx,ts,tsx}',
+  ],
+};
