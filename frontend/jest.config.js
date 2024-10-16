c57579af4068b105c9f571883ae0fc96a879c701
Martin Kalema <kalemaaaaaaaa@gmail.com>
2024-10-16 23:12:58 +0300

    [ref] jest config clean up

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -5,8 +5,8 @@ module.exports = {
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
     testEnvironment: "jsdom", 
     transformIgnorePatterns: ["/node_modules/"],
-    collectCoverage: true,  // Collects coverage
-    coverageDirectory: 'coverage',  // Directory to store coverage results
-    coverageReporters: ['text', 'lcov'],  // Types of reports to generate
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov'],
   };
   
